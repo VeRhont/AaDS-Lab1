6957c4949fe4d1d2cfb9f76662ca5f5f9ad3d5f0
Semyon Ivanov <semeni07@mail.ru>
2023-09-18 22:07:50 +0300

    Binary search

--- Lab 1/binary_search.py
+++ Lab 1/binary_search.py
@@ -1,13 +1,10 @@
-from random import choice
-
-
-def search(array, target):
+def binary_search(array, target):
     steps = 0
     left = 0
     right = len(array) - 1
 
     while left <= right:
-        middle = left + (right - left) // 2
+        middle = (left + right) // 2
 
         if array[middle] == target:
             return steps
@@ -22,21 +19,22 @@ def search(array, target):
 
 
 def main():
-    test_data = [
-        [1, 4, 5, 7, 8, 10, 17, 23, 25, 97, 100, 232],
-        [1, 2, 3, 6, 7, 8, 10, 13, 25, 42, 71, 100, 453, 1204],
-        [1, 4, 5, 7, 8, 10, 17, 23, 24, 97, 100, 232],
-    ]
+    test_data = {
+        4: [1, 4, 5, 7, 8, 10, 17, 23, 25, 97, 100, 232],
+        25: [2, 3, 6, 7, 8, 10, 13, 25, 42, 71, 100, 453, 1204],
+        9: [7, 8, 10, 17, 23, 24, 97, 100, 232],
+    }
 
-    for i in range(len(test_data)):
-        target = choice(test_data[i])
-        result = search(test_data[i], target)
+    for target, array in test_data.items():
+        result = binary_search(array, target)
 
+        print(f'Список: {array}\nЦель: {target}')
         if result != -1:
-            print(f'Потребуется {result} шагов, чтобы найти {target}')
+            print(f'Потребуется {result} шага, чтобы найти элемент {target}')
         else:
             print('Элемент не присутствует в массиве')
 
+        print()
 
 
 if __name__ == '__main__':
--- Lab 1/test_binary_search.py
+++ Lab 1/test_binary_search.py
@@ -1,21 +1,21 @@
-from binary_search import search
+from binary_search import binary_search
 
 
 def test_binary_search():
-    assert search([1, 2, 3, 4], 2) != -1
-    assert search([1, 2, 3, 4, 5, 6, 8], 6) != -1
-    assert search([1, 2, 3, 4], 5) == -1
-    assert search([1, 2, 3, 4], 0) == -1
+    assert binary_search([1, 2, 3, 4], 2) != -1
+    assert binary_search([1, 2, 3, 4, 5, 6, 8], 6) != -1
+    assert binary_search([1, 2, 3, 4], 5) == -1
+    assert binary_search([1, 2, 3, 4], 0) == -1
 
     print('Binary search: OK')
 
 
 def test_steps():
-    assert search([1, 2, 3, 4, 5], 2) == 2
-    assert search([1, 3, 4, 5, 6], 2) == -1
-    assert search([1, 1, 1, 1], 1) == 0
-    assert search([4, 5, 5, 5, 7, 7, 8], 6) == -1
-    assert search([], 1) == -1
+    assert binary_search([1, 2, 3, 4, 5], 2) == 2
+    assert binary_search([1, 3, 4, 5, 6], 2) == -1
+    assert binary_search([1, 1, 1, 1], 1) == 0
+    assert binary_search([4, 5, 5, 5, 7, 7, 8], 6) == -1
+    assert binary_search([], 1) == -1
 
     print('Steps: OK')
 
