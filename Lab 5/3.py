1f04b2493064be8003e8da22454502588c965923
Semyon Ivanov <semeni07@mail.ru>
2023-11-04 16:43:34 +0300

    Started 5th lab

---
+++ Lab 5/3.py
@@ -0,0 +1,12 @@
+from random import randint
+
+
+def main():
+    # n = 10
+    # array = [randint(-100, 100) for i in range(n)]
+
+    array = [4, 5, 6, 1, 2, 5, 4, 8, 9]
+
+
+if __name__ == '__main__':
+    main()
